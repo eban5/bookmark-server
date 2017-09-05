7486ee8d5e53d4d37bd22446ae456b442af75ff8
Esteban Amas <esteban@bluestonelogic.com>
2017-09-04 22:11:51 -0400

    Implements thread-based concurrency

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -19,8 +19,13 @@
 
 import http.server
 import requests
+import threading
+from socketserver import ThreadingMixIn
 import os
 from urllib.parse import unquote, parse_qs
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 memory = {}
 
@@ -121,5 +126,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = http.server.ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
